9cf406666f3745773e2a8d01dd1edf0d292adf54
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-01-09 21:02:40 +0530

    Create amstronginterval.c

---
+++ amstronginterval.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=150,b=160;
+    int r,sum=0,i,c;
+    for(i=a;i<b;i++)
+    {
+    
+        c=i
+    
+        while(i!=0)
+        {
+            r=i%10;
+            sum=sum+r*r*r;
+            i=i/10;
+        
+        }
+        }
+    
+    
+        if(sum==c)
+        {
+            printf("%d",c);
+            
+        }
+       
+    
+    
+
+
+    return 0;
+}
+
+
+
