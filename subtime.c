914a9e91faee76d9e67fdeaeb5623e5ff3633233
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-01-21 12:27:01 +0530

    Create subtime.c

---
+++ subtime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    
+int h1,h2,m1,m2,hr,mi;
+printf("fist hr");
+scanf("%d\t%d",&h1,&m1);
+printf("second hr");
+scanf("%d\t%d",&h2,&m2);
+hr=h1-h2;
+mi=m1-m2;
+printf("%d\t%d",hr,mi);
+
+ 
+    return 0;
+}
+
+
+
