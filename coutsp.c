130ad04a16d9146f1bf017f6f802c80eeb5c2394
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-01-25 16:39:14 +0530

    Create coutsp.c

---
+++ coutsp.c
@@ -0,0 +1,39 @@
+int main()
+{
+    char    str[100];
+    int countDigits,countAlphabet,countSpecialChar,countSpaces;
+    int count;
+ 
+  
+    countDigits=countAlphabet=countSpecialChar=countSpaces=0;
+ 
+    printf("Enter a string: ");
+    gets(str);
+ 
+    for(countr=0;str[count]!=NULL;count++)
+    {
+ 
+        if(str[count]>='0' && str[count]<='9')
+            countDigits++;
+        else if((str[count]>='A' && str[count]<='Z')||(str[count]>='a' && str[count]<='z'))
+            countAlphabet++;
+        else if(str[count]==' ')
+            countSpaces++;
+        else
+            countSpecialChar++;
+    }
+ 
+    printf("\nDigits: %d \nAlphabets: %d \nSpaces: %d \nSpecial Characters: %d",countDigits,countAlphabet,countSpaces,countSpecialChar);
+ 
+    return 0;
+}
+
+
+  
+
+
+
+  
+
+ 
+
