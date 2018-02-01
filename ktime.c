8d5017aa2fea1014a98a276cbe189c0b28e02be3
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-01 09:03:22 +0000

    Create ktime.c

---
+++ ktime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[33];
+int j,k;
+printf("enter the k");
+scanf("%d",&k);
+printf("enter the char");
+scanf("%s",&a);
+for(j=0;j<=k;j++)
+{
+printf("%s",&a);
+}
+return 0;
+}
