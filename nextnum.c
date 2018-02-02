f83b1da7d9cb939801881c2bb202440359eb779c
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-02 14:38:23 +0530

    Create nextnum.c

---
+++ nextnum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+   int n;
+   printf("enter the number:\n");
+   scanf("%d",&n);
+  
+
+   if(n<100000)
+   {
+    
+   
+  
+       printf("\n nextnumber:%d",n+1);
+   
+}
+}
+
+
+
