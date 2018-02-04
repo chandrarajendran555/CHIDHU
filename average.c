6b8f684a304cb809b3521bf5ae91ca44d0bc5227
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-04 11:30:27 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char a[5];
+int n,i,sum=0,c;
+scanf("%d",&n);
+
+printf("enter the elements are:");
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+c=sum/n;
+}
+printf("%d",b);
+}
