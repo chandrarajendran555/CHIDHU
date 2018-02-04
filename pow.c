aa1aa22934c2c46102a3bf81567099c449017bc1
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-04 11:53:22 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,i,c=0,b;
+printf("ENTER THE POWER VALUE\n");
+scanf("%d",&n);
+
+for(i=0;i<n;i++)
+{
+  b=pow(2,i);
+}
+if(n==a)
+{
+printf("YES");
+c++;
+}
+}
