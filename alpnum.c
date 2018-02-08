60f82848e85d322a9d590e4ca6fb98e94bb3fa9c
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-08 11:28:32 +0000

    Create alpnum.c

---
+++ alpnum.c
@@ -0,0 +1,17 @@
+include<stdio.h>
+void main()
+{
+char str;
+printf("Enter the string:");
+scanf("%c",&st);
+printf("Is the string containing alphabets and numbers:");
+if(st>='A' && st<='Z' && st>='0' && st<='9' || st>='a' && st<='z' && stt>='0' && st<='9')
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+
+ 
