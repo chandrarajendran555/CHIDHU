7e8a5ed46da248404860bcf0eb6c4ad68e20ae9a
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-08 11:23:05 +0000

    Create mul add even.c

---
+++ mul add even.c
@@ -0,0 +1,16 @@
+  #include <stdio.h>
+void main()
+{
+	int a,b,mult;
+	scanf("%d%d",&a,&b);
+	mult=a*b;
+	if(mult%2==0)
+{
+printf("the number is even");
+}
+else
+{
+	printf("the number is odd");
+}
+}
+
