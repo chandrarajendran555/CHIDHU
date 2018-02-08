59b870d33e9ce8ab0a6c66ef9de5a7c705580a1a
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-08 11:05:17 +0000

    Create nearest.c

---
+++ nearest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+  int n;
+  printf("\n enter the number:");
+  scanf("%d",&n);
+  if(n%2!=0)
+  {
+    printf("\n nearest even number is: %d",y-1);
+  }
+  else
+  {
+    printf("\n nearest even number is: %d",y-2);
+  }
+  return 0;
+}
