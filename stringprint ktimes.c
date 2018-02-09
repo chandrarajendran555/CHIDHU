524dad1e973ceabcef4681b7e162d4f1db3caf51
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-09 15:06:26 +0530

    Create stringprint ktimes.c

---
+++ stringprint ktimes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int k,i;
+    char c[100];
+    printf("enter the string:");
+    scanf("%s%d",c,&k);
+    for(i=0;i<k;i++)
+    {
+        
+        printf("%c",c[i]);
+    
+    
+}
+}
