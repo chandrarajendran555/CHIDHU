e261fb4bc49b4dfbde64f43792f3c5dbc1c7ddf9
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-22 16:13:18 +0530

    Create mul of 7.c

---
+++ mul of 7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,mul;
+    printf("enter the number:");
+    scanf("%d",&n);
+
+if(n%7==0)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+    
+}
