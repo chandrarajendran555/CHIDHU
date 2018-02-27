0f8dd8bca41d48b9babf89b90bdde664dd919f47
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-02-27 16:43:13 +0530

    Create srt*.c

---
+++ srt*.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[100];
+    int i,n;
+    printf("enter the string: ");
+    scanf("%s",s);
+    n=strlen(s);
+    if(n/2==0)
+    {
+        s[(n/2)-1]='*';
+    }
+    else
+    {
+        s[n/2]='*';
+    }
+    printf("%s",s);
+}
