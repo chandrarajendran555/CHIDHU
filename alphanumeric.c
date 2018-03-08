23b280a27f7f19f74db4193f1b3bf3e92ddf8aad
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-03-08 16:33:30 +0530

    Create alphanumeric.c

---
+++ alphanumeric.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int n,i;
+    printf("enter the string");
+    scanf("%s",a);
+    n=strlen(a);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>='0' && a[i]<='9')
+        {
+            printf("%c",a[i]);
+            
+        }
+    }
+}
