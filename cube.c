daaa9cd0384043983a41863394803b3980450f83
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-03-13 15:43:09 +0530

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int l,w,h,vol;
+    printf("enter the hight width,length:");
+    scanf("%d%d%d",&h,&w,&l);
+    vol=h*l*w;
+    printf("the volume of cuboid:%d",vol);
+}
+    
