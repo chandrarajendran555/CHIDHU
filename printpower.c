817455c966b245d2fc051cbcba85bd3cd65d856f
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-03-16 15:04:51 +0530

    Rename power.c to printpower.c

---
+++ printpower.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,c,i;
+    printf("enter the number:");
+    scanf("%d",&n);
+    printf("enter the number:");
+    scanf("%d",&k);
+    for(i=1;i<=k;i++)
+    {
+        c=i*n;
+    }
+    printf("%d",c);
+   
+    }
