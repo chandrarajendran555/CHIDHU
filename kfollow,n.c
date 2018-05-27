22a60c2ad4985ad2ff101e479f25ee4dd1431ff5
chandrarajendran555 <31505003+chandrarajendran555@users.noreply.github.com>
2018-05-27 18:44:58 +0530

    Create kfollow,n.c

---
+++ kfollow,n.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,i,a[100];
+    printf("enter the number:");
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+if(k<=a[i])
+{
+    printf("yes");
+    
+}
+else
+{
+    printf("no");
+}
+}
